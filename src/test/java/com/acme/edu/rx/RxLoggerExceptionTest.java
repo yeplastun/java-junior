b0f835d61596a30c4cae9a28f8df645be33360a4
Egor <yeplastun@gmail.com>
2017-09-05 01:52:14 +0300

    added test

--- src/test/java/com/acme/edu/rx/RxLoggerExceptionTest.java
+++ src/test/java/com/acme/edu/rx/RxLoggerExceptionTest.java
@@ -1,5 +1,6 @@
 package com.acme.edu.rx;
 
+import com.acme.edu.rx.exception.IOLogMessageException;
 import com.acme.edu.rx.exception.InvalidLogMessageException;
 import com.acme.edu.rx.exception.LogMessageException;
 import com.acme.edu.rx.formatter.TestLogFormatter;
@@ -56,5 +57,21 @@ public class RxLoggerExceptionTest {
 
         // When, then
         logger.log(null);
+    }
+
+    @Test
+    public void shouldTrowSeveralExceptions() {
+        // Given
+        RxLogger logger = new RxLogger(new TestLogFormatter(), new ExceptionTestSaver());
+        logger.getExceptionStream().subscribe(exceptionObserver);
+        // When
+        logger.log(1);
+        logger.flush();
+        logger.log("");
+        logger.flush();
+
+        // Then
+        exceptionObserver.assertValueAt(0, ex -> ex.getClass() == IOLogMessageException.class);
+        exceptionObserver.assertValueAt(1, ex -> ex.getClass() == InvalidLogMessageException.class);
     }
 }
