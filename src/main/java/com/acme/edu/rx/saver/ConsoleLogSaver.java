5a85a0fdff3fb08588674a52d16c5dca02317fcd
ignorer <ignorer@bk.ru>
2017-09-05 17:13:26 +0300

    docs

--- src/main/java/com/acme/edu/rx/saver/ConsoleLogSaver.java
+++ src/main/java/com/acme/edu/rx/saver/ConsoleLogSaver.java
@@ -1,13 +1,12 @@
 package com.acme.edu.rx.saver;
 
-import com.acme.edu.rx.exception.IOLogMessageException;
-
 /**
  * Class for saving logging messages to the console.
  */
 public class ConsoleLogSaver implements LogSaver {
     /**
      * Saves message to the console.
+     *
      * @param message Message to be saved.
      */
     @Override
